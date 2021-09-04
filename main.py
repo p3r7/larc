3b69815f3d8e87cb2e87918c48464bc3e404fa04
p3r7 <11557146+p3r7@users.noreply.github.com>
2021-09-04 21:24:01 +0200

    way cleaner & working code

--- main.py
+++ main.py
@@ -5,6 +5,29 @@ from time import sleep
 
 gpio.setwarnings(False)
 
+
+## ------------------------------------------------------------------------
+## BINARY HELPERS
+
+def hexp(v):
+    padding = 8
+    return '0x{0:0{1}X}'.format(v, padding)
+
+def binp(v):
+    return format(v, '#010b')
+
+def is_bit_on(v, index):
+    mask = 0b1 << index
+    return (mask & v) > 0
+
+### NB: on my specific variant of the 74HC595, Q pin indexes are all reversed...
+def normalize_bit_index(index):
+    return 7 - index
+
+
+## ------------------------------------------------------------------------
+
+
 wrt1414 = 15
 # write delay
 wrt1414_twd = 20 // 1000000000
@@ -16,6 +39,7 @@ wrt1414_tas = 150 // 1000000000
 # address hold time
 wrt1414_tah = 50 // 1000000000
 
+
 class Shifter():
 
     # 14 - DS / SER    - serial data input          -  data pin
@@ -23,7 +47,7 @@ class Shifter():
     # 11 - SH_CP / SCK - shift register clock pin   -  clock pin
     clock=12
     # 12 - ST_CP / RCK - storage register clock pin -  latch pin
-    clearPin=13
+    latch=13
 
     # NB:
     # 13 - OE / G      (output enabled)  -> on GND to enable
@@ -33,33 +57,43 @@ class Shifter():
         self.setupBoard()
         self.pause=0
     def tick(self):
-        gpio.output(Shifter.clock,gpio.HIGH)
+        gpio.output(Shifter.clock, gpio.HIGH)
         sleep(self.pause)
-        gpio.output(Shifter.clock,gpio.LOW)
+        gpio.output(Shifter.clock, gpio.LOW)
         sleep(self.pause)
 
+    # def setValue(self,value):
+    #     for i in range(24):
+    #         bitwise=0x800000>>i
+    #         bit=bitwise&value
+    #         if (bit==0):
+    #             gpio.output(Shifter.inputB,gpio.LOW)
+    #         else:
+    #             gpio.output(Shifter.inputB,gpio.HIGH)
+    #         Shifter.tick(self)
     def setValue(self,value):
-        for i in range(24):
-            bitwise=0x800000>>i
-            bit=bitwise&value
-            if (bit==0):
-                gpio.output(Shifter.inputB,gpio.LOW)
+        gpio.output(Shifter.latch, gpio.LOW)
+        for r in range(8):
+            r = normalize_bit_index(r) ## software fix for some variants of the shift register
+            if (is_bit_on(value, r)):
+                gpio.output(Shifter.inputB, gpio.HIGH)
             else:
-                gpio.output(Shifter.inputB,gpio.HIGH)
+                gpio.output(Shifter.inputB,gpio.LOW)
             Shifter.tick(self)
+        gpio.output(Shifter.latch, gpio.HIGH)
 
     def clear(self):
-        gpio.output(Shifter.clearPin,gpio.LOW)
+        gpio.output(Shifter.latch, gpio.LOW)
         Shifter.tick(self)
-        gpio.output(Shifter.clearPin,gpio.HIGH)
+        gpio.output(Shifter.latch, gpio.HIGH)
 
     def setupBoard(self):
-        gpio.setup(Shifter.inputB,gpio.OUT)
-        gpio.output(Shifter.inputB,gpio.LOW)
-        gpio.setup(Shifter.clock,gpio.OUT)
-        gpio.output(Shifter.clock,gpio.LOW)
-        gpio.setup(Shifter.clearPin,gpio.OUT)
-        gpio.output(Shifter.clearPin,gpio.HIGH)
+        gpio.setup(Shifter.inputB, gpio.OUT)
+        gpio.output(Shifter.inputB, gpio.LOW)
+        gpio.setup(Shifter.clock, gpio.OUT)
+        gpio.output(Shifter.clock, gpio.LOW)
+        gpio.setup(Shifter.latch, gpio.OUT)
+        gpio.output(Shifter.latch, gpio.HIGH)
 
 
 def write_value(shifter, value):
@@ -79,100 +113,34 @@ def write_value(shifter, value):
 
 
 def main():
-    pause=0.4
+    pause=0.2
     gpio.setmode(gpio.BOARD)
     shifter=Shifter()
     running=True
 
     gpio.setup(wrt1414, gpio.OUT)
 
-    # start_v = 0x0000000
-    # end_v = 0x0FFFFFF
-
-    # start_v = 0x11 # 17
-    # end_v = 0x2f   # 47
-
-    # start_v = 0x50
-    # end_v = 0x0FFFFFF
-
-    # start_v = 0x52
-    # end_v = 0x70
+    shifter.clear()
 
 
+    char_rows_prefixes = [0b010, 0b011, 0b100, 0b101]
+    char_row_index = 0
+    digit_sel_prefixes = [0b00, 0b01, 0b10, 0b11]
 
-    start_v = 0b0100001
-    end_v =   0b0101111
-
-    start_v = 0b10000000
-    end_v =   0b10000000
-
-
-    # - 0b0100000 - " "
-    # - 0b0100001 - inverted F
-    # - 0b0100010 - $
-
-    # 0
-    # expected 0b0110000
-    # actual   0b0101000
-
-    # 3
-    # expected 0b0110011
-    # actual   0b0101001
-    # -> wtf?
-
-
-
-    v = start_v
-
+    char_col = 0b0000
     while running==True:
         try:
 
-            print(bin(v) + ' | ' + hex(v) + ' | ' + str(v))
+            v = (char_rows_prefixes[char_row_index] << 4) + char_col
+
+            print(binp(v) + ' | ' + hex(v) + ' | ' + str(v))
             write_value(shifter, v)
             sleep(pause)
 
-            v += 1
-            if v > end_v:
-                v = start_v
-
-            # write_value(shifter, 1)
-            # sleep(pause)
-
-            # print("switch")
-
-            # write_value(shifter, 0x0AAAAAA)
-            # sleep(pause)
-
-            # print("switch")
-
-            # write_value(shifter, 0x0555555)
-            # sleep(pause)
-
-            # print("switch")
-
-
-
-            # gpio.output(wrt1414, gpio.LOW)
-            # sleep(wrt1414_twd)
-
-            # shifter.clear()
-            # shifter.setValue(1)
-            # # shifter.setValue(0x0AAAAAA)
-            # # shifter.setValue(0x0555555)
-
-            # # sleep(1)
-            # sleep(wrt1414_tw)
-
-            # sleep(wrt1414_tah)
-            # gpio.output(wrt1414, gpio.HIGH)
-
-
-            # # shifter.clear()
-            # # shifter.setValue(0x0AAAAAA)
-            # # sleep(pause)
-            # # shifter.clear()
-            # # shifter.setValue(0x0555555)
-            # # sleep(pause)
+            char_col += 1
+            if char_col > 0b1111:
+                char_col = 0b0000
+                char_row_index = (char_row_index + 1) % 4
 
         except KeyboardInterrupt:
             running=False
